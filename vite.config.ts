cd43c917c6ffa5f9f1289dac46e54b42715b9222
Micro Skulphan <micro.skulphan@everymatrix.com>
2026-02-15 14:05:31 +0700

    fix: public app 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        public: resolve(__dirname, 'public.html'),
+      },
+    },
+  },
   plugins: [
     react(),
     {
